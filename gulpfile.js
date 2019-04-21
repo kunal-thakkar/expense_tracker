0d1e1ca47ee3c3df25c1b804d2d33a958aee43c4
Kunal <kunalmthakkar@gmail.com>
2019-04-21 06:52:13 +0530

    Modified gulp to watch html files and copy to dest and minor improvements

--- gulpfile.js
+++ gulpfile.js
@@ -1,57 +1,76 @@
 var gulp = require('gulp');
 var concat = require('gulp-concat');
 
-gulp.task('scripts', function() {
-	return gulp.src([
-	    'public/js/jquery-3.3.1.min.js',
-		'public/js/flot/jquery.canvaswrapper.js',
-		'public/js/flot/jquery.colorhelpers.js',
-		'public/js/flot/jquery.flot.js',
-		'public/js/flot/jquery.flot.saturated.js',
-		'public/js/flot/jquery.flot.browser.js',
-		'public/js/flot/jquery.flot.drawSeries.js',
-		'public/js/flot/jquery.flot.errorbars.js',
-		'public/js/flot/jquery.flot.uiConstants.js',
-		'public/js/flot/jquery.flot.logaxis.js',
-		'public/js/flot/jquery.flot.symbol.js',
-		'public/js/flot/jquery.flot.flatdata.js',
-		'public/js/flot/jquery.flot.navigate.js',
-		'public/js/flot/jquery.flot.fillbetween.js',
-		'public/js/flot/jquery.flot.stack.js',
-		'public/js/flot/jquery.flot.touchNavigate.js',
-		'public/js/flot/jquery.flot.hover.js',
-		'public/js/flot/jquery.flot.touch.js',
-		'public/js/flot/jquery.flot.time.js',
-		'public/js/flot/jquery.flot.axislabels.js',
-		'public/js/flot/jquery.flot.selection.js',
-		'public/js/flot/jquery.flot.composeImages.js',
-		'public/js/flot/jquery.flot.legend.js',
-		'public/js/flot/jquery.flot.pie.js',
-		// 'public/js/moment.js',
-		'public/js/jquery.dataTables.min.js',
-		'public/js/dataTables.responsive.min.js',
-		'public/js/angular.min.js',
-		'public/js/angular-route.js',
-		'public/js/main.js',
-		'public/js/util.js',
-		'public/js/directive.js',
-		'public/js/dashboard.js',
-		'public/js/transaction.js'		
-	])
+var configuration = {
+    paths: {
+        src: {
+            html: './public/**/*.html',
+            css: [
+                'public/css/jquery.dataTables.min.css',
+                'public/css/responsive.dataTables.min.css',
+                'public/css/styles.css'
+            ],
+            js: [
+                'public/js/jquery-3.3.1.min.js',
+                'public/js/flot/jquery.canvaswrapper.js',
+                'public/js/flot/jquery.colorhelpers.js',
+                'public/js/flot/jquery.flot.js',
+                'public/js/flot/jquery.flot.saturated.js',
+                'public/js/flot/jquery.flot.browser.js',
+                'public/js/flot/jquery.flot.drawSeries.js',
+                'public/js/flot/jquery.flot.errorbars.js',
+                'public/js/flot/jquery.flot.uiConstants.js',
+                'public/js/flot/jquery.flot.logaxis.js',
+                'public/js/flot/jquery.flot.symbol.js',
+                'public/js/flot/jquery.flot.flatdata.js',
+                'public/js/flot/jquery.flot.navigate.js',
+                'public/js/flot/jquery.flot.fillbetween.js',
+                'public/js/flot/jquery.flot.stack.js',
+                'public/js/flot/jquery.flot.touchNavigate.js',
+                'public/js/flot/jquery.flot.hover.js',
+                'public/js/flot/jquery.flot.touch.js',
+                'public/js/flot/jquery.flot.time.js',
+                'public/js/flot/jquery.flot.axislabels.js',
+                'public/js/flot/jquery.flot.selection.js',
+                'public/js/flot/jquery.flot.composeImages.js',
+                'public/js/flot/jquery.flot.legend.js',
+                'public/js/flot/jquery.flot.pie.js',
+                // 'public/js/moment.js',
+                'public/js/jquery.dataTables.min.js',
+                'public/js/dataTables.responsive.min.js',
+                'public/js/angular.min.js',
+                'public/js/angular-route.js',
+                'public/js/main.js',
+                'public/js/util.js',
+                'public/js/directive.js',
+                'public/js/dashboard.js',
+                'public/js/transaction.js'		
+            ]
+        },
+        dist: './src/main/resources/static'
+    }
+};
+
+// Gulp task to copy HTML files to output directory
+gulp.task('html', function() {
+    return gulp.src(configuration.paths.src.html)
+        .pipe(gulp.dest(configuration.paths.dist));
+});
+
+gulp.task('minify-js', function() {
+	return gulp.src(configuration.paths.src.js)
     .pipe(concat('app.js'))
-    .pipe(gulp.dest('./src/main/resources/static/js'));
+    .pipe(gulp.dest(configuration.paths.dist + '/js'));
 });
 
-gulp.task('minify-css', function(){
-	return gulp.src([
-		'public/css/jquery.dataTables.min.css',
-		'public/css/responsive.dataTables.min.css',
-		'public/css/styles.css'
-	])
-	.pipe(concat("style.css"))
-	.pipe(gulp.dest('./src/main/resources/static/css'));
-})
-
-gulp.task('default', function () {
-	return gulp.watch('public/**/*.*', gulp.parallel('scripts', 'minify-css'));
+gulp.task('minify-css', function () {
+   return gulp.src(configuration.paths.src.css)
+       .pipe(concat('style.css'))
+       .pipe(gulp.dest(configuration.paths.dist + '/css'))
 });
+
+gulp.task('default', gulp.series('html', 'minify-js', 'minify-css', function () {
+	gulp.watch(configuration.paths.src.js, ()=>'minify-js');
+	gulp.watch(configuration.paths.src.html, ()=>'html');
+    gulp.watch(configuration.paths.src.css, ()=>'minify-css');
+}));
